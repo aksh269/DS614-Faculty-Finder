e4c07dcf9527b94de7334baaf93ad54eec069237
Sanjana006 <sanjananathani06@gmail.com>
2026-02-06 19:47:41 +0530

    Minor changes

--- DS614-Faculty-Finder/DS614-Faculty-Recommender/scripts/build_index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from recommender.index_builder import build_index
-
-if __name__ == "__main__":
-    build_index()
